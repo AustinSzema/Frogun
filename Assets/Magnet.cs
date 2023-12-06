71a24d113ff2c9f886c93da5b52fe118ce75d335
Austin Szema <aszema16@gmail.com>
2023-12-06 01:48:17 -0500

    Added multiple magnet positions

--- Assets/Magnet.cs
+++ Assets/Magnet.cs
@@ -10,30 +10,78 @@ public class Magnet : MonoBehaviour
     private List<Rigidbody> _magneticObjects = new List<Rigidbody>();
 
 
+    [SerializeField] private Transform _footPosition;
+
+    [SerializeField] private Transform _handPosition;
+
+    [SerializeField] private Transform _explodePosition;
+
+    
     [SerializeField]
     private GameObject _cubePrefab;
         
     private void Start()
     {
-        for (int i = 0; i < 100; i++)
+        /*for (int i = 0; i < 100; i++)
         {
             GameObject cube = Instantiate(_cubePrefab, transform.position + new Vector3(Random.Range(-100f, 100f), Random.Range(-100f, 100f), Random.Range(-100f, 100f)), Quaternion.identity);
             _magneticObjects.Add(cube.GetComponent<Rigidbody>());
+            
+        }*/
+        
+        Magnetic[] magneticObjects = GameObject.FindObjectsOfType<Magnetic>();
+
+        // Do something with each object that has the Magnetic script
+        foreach (Magnetic magneticObject in magneticObjects)
+        {
+            // Perform actions on each object, for example:
+            _magneticObjects.Add(magneticObject.GetComponent<Rigidbody>());
         }
+        
+
     }
 
     private bool _activateMagnet = false;
     
     private void Update()
     {
-        if (Input.GetMouseButton(1))
+        if (Input.GetKey(KeyCode.Space))
         {
-            _activateMagnet = true;
+            transform.position = _footPosition.position;
         }
         else
         {
-            _activateMagnet = false;
+            transform.position = _handPosition.position;
         }
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+            _activateMagnet = true;
+            transform.position = _handPosition.position;
+        }
+        if(Input.GetMouseButtonUp(1))
+        {
+            _activateMagnet = false;
+            /*foreach (Rigidbody rb in _magneticObjects)
+            {
+                transform.position = _explodePosition.position;
+                rb.AddExplosionForce(20000f, transform.position, 100f, 0.0F);
+            }*/
+        }
+
+        if (Input.GetMouseButton(1) && Input.GetMouseButtonDown(0))
+        {
+            foreach (Rigidbody rb in _magneticObjects)
+            {
+                
+                rb.AddForce(transform.forward * 5000f);
+                
+                _activateMagnet = false;
+            }
+        }
+
+
+        
     }
 
     // Update is called once per frame
@@ -43,7 +91,14 @@ public class Magnet : MonoBehaviour
         {
             foreach (Rigidbody obj in _magneticObjects )
             {
+                obj.velocity = Vector3.zero;
                 obj.position = Vector3.MoveTowards(obj.transform.position, transform.position, Time.deltaTime * 50f);
+
+                /*// Calculate the direction from the current position to the target position
+                Vector3 direction = (transform.position - obj.position).normalized;
+
+                // Set the velocity of the Rigidbody in the calculated direction
+                obj.velocity = Time.fixedDeltaTime * 2000f * direction;*/
             }
             
         }
--- Assets/Magnetic.cs
+++ Assets/Magnetic.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,5 +15,13 @@ public class Magnetic : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.GetComponent<IDamageable>() != null)
+        {
+            other.gameObject.GetComponent<IDamageable>().takeDamage(1f);
+        }
     }
 }
