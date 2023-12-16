6ae7434a0c9121ac8dc59901f3a22352ff7c3fab
Austin Szema <aszema16@gmail.com>
2023-12-16 03:21:16 -0500

    Updated audio and added in different music

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -7,7 +7,11 @@ using Random = UnityEngine.Random;
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSource;
+    /*
     [SerializeField] private AudioClip _hitClip;
+    [SerializeField] private AudioClip _bigHitClip;
+    */
+
 
     public Queue<AudioClip> _queue = new Queue<AudioClip>();
 
@@ -19,7 +23,12 @@ public class AudioManager : MonoBehaviour
         {
             _queue.Enqueue(_clip);
             _lastPosition = position;
-        }
+        }/*
+        else
+        {
+            _queue.Enqueue(_bigHitClip);
+            _lastPosition = position;
+        }*/
     }
 
 
@@ -27,14 +36,24 @@ public class AudioManager : MonoBehaviour
     {
         if (_queue.Count > 0)
         {
-            _audioSource.PlayOneShot(_queue.Dequeue());
+            /*if (_queue.Dequeue().name == _bigHitClip.name)
+            {
+                _audioSource.pitch = 1f;
+                _audioSource.PlayOneShot(_queue.Dequeue());
+            }
+            else
+            {
+                _audioSource.pitch = Random.Range(0.5f, 1.5f);
+                _audioSource.PlayOneShot(_queue.Dequeue());
+            }*/
+            PlayHitSound();
         }
     }
 
     public void PlayHitSound()
     {
-        _audioSource.pitch = Random.Range(0.5f, 1.5f);
-        _audioSource.PlayOneShot(_hitClip);
+        _audioSource.pitch = Random.Range(0.8f, 1.2f);
+        _audioSource.PlayOneShot(_queue.Dequeue());
     }
 
 
