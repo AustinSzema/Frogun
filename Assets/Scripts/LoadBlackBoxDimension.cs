24ac6c00216cc127c54b68c11e4a1111c6b9dbb4
Austin Szema <aszema16@gmail.com>
2024-01-12 01:31:25 -0500

    Added black box dimension

---
+++ Assets/Scripts/LoadBlackBoxDimension.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadBlackBoxDimension : MonoBehaviour
+{
+    [SerializeField] private int _blackBoxDimensionSceneIndex;
+
+    [SerializeField] private intVariable _previousDimensionSceneIndex;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (SceneManager.GetActiveScene().buildIndex != _blackBoxDimensionSceneIndex)
+            {
+                SceneManager.LoadScene(_blackBoxDimensionSceneIndex);
+            }
+            else
+            {
+                SceneManager.LoadScene(_previousDimensionSceneIndex.Value);
+                //SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
